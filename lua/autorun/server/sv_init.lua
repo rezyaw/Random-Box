b22b31291bd84f5e94d32c2d45d9d89de743008e
WayZer <33715244+rezyaw@users.noreply.github.com>
2018-01-05 01:18:06 +0300

    Update sv_init.lua
    Add commit

--- lua/autorun/server/sv_init.lua
+++ lua/autorun/server/sv_init.lua
@@ -1,20 +1,20 @@
-local prize = {
-	[1] = "csgo_bayonet",
-	[2] = "weapon_vape_hallucinogenic",
-	[3] = "guitar_stalker",
-	[4] = "weapon_vape_medicinal",
-	[5] = "weapon_vape_juicy",
-	[6] = "ptp_cs_glock",
-	[7] = "ptp_cs_p228",
-	[8] = "weapon_vape",
-	[9] = "ptp_cs_scout",
-	[10] = "guitar",
-	[11] = "ptp_cs_deagle",
-	[12] = "weapon_357",
-	[13] = "csgo_bayonet",
-	[14] = "weapon_357",
-	[15] = "csgo_bayonet",
-	[16] = "ptp_cs_p90",
+local prize = { 
+	[1] = "your_weapon_prize",
+	[2] = "your_weapon_prize",
+	[3] = "your_weapon_prize",
+	[4] = "your_weapon_prize",
+	[5] = "your_weapon_prize",
+	[6] = "your_weapon_prize",
+	[7] = "your_weapon_prize",
+	[8] = "your_weapon_prize",
+	[9] = "your_weapon_prize",
+	[10] = "your_weapon_prize",
+	[11] = "your_weapon_prize",
+	[12] = "your_weapon_prize",
+	[13] = "your_weapon_prize",
+	[14] = "your_weapon_prize",
+	[15] = "your_weapon_prize",
+	[16] = "your_weapon_prize",
 }
 
 util.AddNetworkString("giveprice")
@@ -25,10 +25,10 @@ net.Receive("giveprice", function(len, ply)
 	if use < 3 then 
    	 ply:ChatPrint("You did not activate all the cases")
    	else
-	  if ply:getDarkRPVar("money") < 15000 then
+	  if ply:getDarkRPVar("money") < 15000 then -- change the price
 	  ply:ChatPrint("You do not have enough money to play")
 	  else
-	  ply:addMoney(-15000)
+	  ply:addMoney(-15000) -- change the price
 	  ply:Give(prize[math.random(1,16)])
 	  ply:Give(prize[math.random(1,16)])
 	  ply:Give(prize[math.random(1,16)])
